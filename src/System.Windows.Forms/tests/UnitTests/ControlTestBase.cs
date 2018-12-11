931fb0bd7067b12c6727e5906ae218d052b61a95
Sam Harwell <sam@tunnelvisionlabs.com>
2018-12-11 09:33:37 -0600

    checkpoint

---
+++ src/System.Windows.Forms/tests/UnitTests/ControlTestBase.cs
@@ -0,0 +1,68 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Threading;
+using Xunit;
+
+namespace System.Windows.Forms.Tests
+{
+    public abstract class ControlTestBase : IAsyncLifetime
+    {
+        private DenyExecutionSynchronizationContext _denyExecutionSynchronizationContext;
+        private JoinableTaskCollection _joinableTaskCollection;
+
+        protected JoinableTaskContext JoinableTaskContext { get; private set; }
+
+        protected JoinableTaskFactory JoinableTaskFactory { get; private set; }
+
+        public virtual Task InitializeAsync()
+        {
+            if (Thread.CurrentThread.GetApartmentState() == ApartmentState.STA)
+            {
+                JoinableTaskContext = new JoinableTaskContext();
+            }
+            else
+            {
+                _denyExecutionSynchronizationContext = new DenyExecutionSynchronizationContext(SynchronizationContext.Current);
+                JoinableTaskContext = new JoinableTaskContext(_denyExecutionSynchronizationContext.MainThread, _denyExecutionSynchronizationContext);
+            }
+
+            _joinableTaskCollection = JoinableTaskContext.CreateCollection();
+            JoinableTaskFactory = JoinableTaskContext.CreateFactory(_joinableTaskCollection);
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task DisposeAsync()
+        {
+            await _joinableTaskCollection.JoinTillEmptyAsync();
+            JoinableTaskContext = null;
+            JoinableTaskFactory = null;
+            if (_denyExecutionSynchronizationContext != null)
+            {
+                SynchronizationContext.SetSynchronizationContext(_denyExecutionSynchronizationContext.UnderlyingContext);
+                _denyExecutionSynchronizationContext.ThrowIfSwitchOccurred();
+            }
+        }
+
+        protected async Task WaitForIdleAsync()
+        {
+            var idleCompletionSource = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+            Application.Idle += HandleApplicationIdle;
+
+            // Queue an event to make sure we don't stall if the application was already idle
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
+            await Task.Yield();
+
+            await idleCompletionSource.Task;
+            Application.Idle -= HandleApplicationIdle;
+
+            void HandleApplicationIdle(object sender, EventArgs e)
+            {
+                idleCompletionSource.TrySetResult(default);
+            }
+        }
+    }
+}
