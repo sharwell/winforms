c86ac1ceed3abe57c0ce35421d5fe5565d5e6311
Sam Harwell <sam@tunnelvisionlabs.com>
2018-12-12 10:52:52 -0600

    Add tests for button positioning after form resize

--- src/System.Windows.Forms/tests/UnitTests/ButtonUI.cs
+++ src/System.Windows.Forms/tests/UnitTests/ButtonUI.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Drawing;
 using System.Threading.Tasks;
 using WindowsInput.Native;
 using Xunit;
@@ -104,6 +105,114 @@ namespace System.Windows.Forms.Tests
 
                 Assert.Equal(DialogResult.Cancel, form.DialogResult);
                 Assert.False(form.Visible);
+            });
+        }
+
+        [WinFormsFact]
+        public async Task NoResizeOnWindowSizeWiderAsync()
+        {
+            await RunSingleControlTestAsync<Button>(async (form, control) =>
+            {
+                var originalFormSize = form.DisplayRectangle.Size;
+                var originalButtonPosition = control.DisplayRectangle;
+
+                var mouseDragHandleOnForm = new Point(form.DisplayRectangle.Right, form.DisplayRectangle.Top + form.DisplayRectangle.Height / 2);
+                await MoveMouseAsync(form, form.PointToScreen(mouseDragHandleOnForm));
+
+                await InputSimulator.SendAsync(
+                    form,
+                    inputSimulator => inputSimulator.Mouse
+                        .LeftButtonDown()
+                        .MoveMouseBy(form.DisplayRectangle.Width, 0)
+                        .LeftButtonUp());
+
+                Assert.True(form.DisplayRectangle.Width > originalFormSize.Width);
+                Assert.Equal(originalFormSize.Height, form.DisplayRectangle.Height);
+                Assert.Equal(originalButtonPosition, control.DisplayRectangle);
+            });
+        }
+
+        [WinFormsFact]
+        public async Task NoResizeOnWindowSizeTallerAsync()
+        {
+            await RunSingleControlTestAsync<Button>(async (form, control) =>
+            {
+                var originalFormSize = form.DisplayRectangle.Size;
+                var originalButtonPosition = control.DisplayRectangle;
+
+                var mouseDragHandleOnForm = new Point(form.DisplayRectangle.Left + form.DisplayRectangle.Width / 2, form.DisplayRectangle.Bottom + 1);
+                await MoveMouseAsync(form, form.PointToScreen(mouseDragHandleOnForm));
+
+                await InputSimulator.SendAsync(
+                    form,
+                    inputSimulator => inputSimulator.Mouse
+                        .LeftButtonDown()
+                        .MoveMouseBy(0, form.DisplayRectangle.Height)
+                        .LeftButtonUp());
+
+                Assert.True(form.DisplayRectangle.Height > originalFormSize.Height);
+                Assert.Equal(originalFormSize.Width, form.DisplayRectangle.Width);
+                Assert.Equal(originalButtonPosition, control.DisplayRectangle);
+            });
+        }
+
+        [WinFormsFact]
+        public async Task ResizeOnWindowSizeWiderAsync()
+        {
+            await RunSingleControlTestAsync<Button>(async (form, control) =>
+            {
+                control.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+
+                var originalFormSize = form.DisplayRectangle.Size;
+                var originalButtonPosition = control.DisplayRectangle;
+
+                var mouseDragHandleOnForm = new Point(form.DisplayRectangle.Right, form.DisplayRectangle.Top + form.DisplayRectangle.Height / 2);
+                await MoveMouseAsync(form, form.PointToScreen(mouseDragHandleOnForm));
+
+                await InputSimulator.SendAsync(
+                    form,
+                    inputSimulator => inputSimulator.Mouse
+                        .LeftButtonDown()
+                        .MoveMouseBy(form.DisplayRectangle.Width, 0)
+                        .LeftButtonUp());
+
+                Assert.True(form.DisplayRectangle.Width > originalFormSize.Width);
+                Assert.Equal(originalFormSize.Height, form.DisplayRectangle.Height);
+
+                Assert.Equal(originalButtonPosition.Location, control.DisplayRectangle.Location);
+
+                // Still anchored on right
+                Assert.Equal(originalFormSize.Width - originalButtonPosition.Right, form.DisplayRectangle.Width - control.DisplayRectangle.Right);
+            });
+        }
+
+        [WinFormsFact]
+        public async Task ResizeOnWindowSizeTallerAsync()
+        {
+            await RunSingleControlTestAsync<Button>(async (form, control) =>
+            {
+                control.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+
+                var originalFormSize = form.DisplayRectangle.Size;
+                var originalButtonPosition = control.DisplayRectangle;
+
+                var mouseDragHandleOnForm = new Point(form.DisplayRectangle.Left + form.DisplayRectangle.Width / 2, form.DisplayRectangle.Bottom + 1);
+                await MoveMouseAsync(form, form.PointToScreen(mouseDragHandleOnForm));
+
+                await InputSimulator.SendAsync(
+                    form,
+                    inputSimulator => inputSimulator.Mouse
+                        .LeftButtonDown()
+                        .MoveMouseBy(0, form.DisplayRectangle.Height)
+                        .LeftButtonUp());
+
+                Assert.True(form.DisplayRectangle.Height > originalFormSize.Height);
+                Assert.Equal(originalFormSize.Width, form.DisplayRectangle.Width);
+
+                Assert.Equal(originalButtonPosition.Location, control.DisplayRectangle.Location);
+
+                // Still anchored on bottom
+                Assert.Equal(originalFormSize.Height - originalButtonPosition.Bottom, form.DisplayRectangle.Height - control.DisplayRectangle.Bottom);
             });
         }
     }
